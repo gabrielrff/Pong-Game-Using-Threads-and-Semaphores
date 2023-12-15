edad729c551f889257eb3e6166e688addbb7b4f1
Gabriel Barbosa de Oliveira <gabrieloliveira00@outlook.com>
2023-12-15 13:13:15 -0300

    basic gampeplay and menus working.

--- main.cpp
+++ main.cpp
@@ -9,30 +9,26 @@
 
 using namespace std;
 
-int player_score = 0;
+// globals
+typedef enum GameScreen
+{
+    LOGO = 0,
+    TITLE,
+    GAMEPLAY,
+    ENDING
+} GameScreen;
+
+GameScreen currentScreen = LOGO;
 
 mutex mtx;
 
-// void sem_wait(int *semaphore)
-// {
-//     while ((*semaphore) < 0)
-//     {
-//         cout << *semaphore << endl;
-//         continue;
-//     }
-//     (*semaphore)--;
-// }
-
-// void sem_signal(int *semaphore)
-// {
-//     (*semaphore)++;
-// }
+chrono::duration<double> score;
 
 class Ball
 {
 public:
     float x, y;
-    int speed_x, speed_y;
+    float initial_speed, speed_x, speed_y;
     int radius;
 
     void Draw()
@@ -42,29 +38,38 @@ public:
 
     void Update()
     {
-        x += speed_x;
-        y += speed_y;
+        if (currentScreen == GAMEPLAY)
+        {
 
-        if (y + radius >= GetScreenHeight() || y - radius <= 0)
-        {
-            speed_y *= -1;
-        }
-        if (x + radius >= GetScreenWidth() || x - radius <= 0)
-        {
-            player_score++;
-            ResetBall();
+            x += speed_x;
+            y += speed_y;
+
+            if (y + radius >= GetScreenHeight() || y - radius <= 0)
+            {
+                speed_y *= -1;
+            }
+            if (x + radius >= GetScreenWidth() || x - radius <= 0)
+            {
+                GameOver();
+            }
         }
     }
 
-    void ResetBall()
+    void Reset()
     {
+        // reseting positions
         x = GetScreenWidth() / 2;
         y = GetScreenHeight() / 2;
 
+        // reseting speed (ball goes do random direction)
         int speed_choices[2] = {-1, 1};
-        speed_x *= speed_choices[GetRandomValue(0, 1)]; // Quando resetada a bola vai pra um lado aleatório
-        speed_y *= speed_choices[GetRandomValue(0, 1)];
-        // Tratar a hora de resetar a bola pra ver qual retangulo é pra mexer
+        speed_x = initial_speed * speed_choices[GetRandomValue(0, 1)];
+        speed_y = initial_speed * speed_choices[GetRandomValue(0, 1)];
+    }
+
+    void GameOver()
+    {
+        currentScreen = ENDING;
     }
 };
 
@@ -73,7 +78,7 @@ class Paddle
 public:
     float x, y;
     float width, heigth;
-    int speed;
+    float initial_speed, speed;
     bool wasd = false;
 
     void Draw()
@@ -84,14 +89,14 @@ public:
     void Update()
     {
         if ((wasd && IsKeyDown(KEY_W)) || (!wasd && IsKeyDown(KEY_I)))
-        { // Aqui onde mexemos os retângulos
-            y = y - speed;
-            this_thread::sleep_for(chrono::milliseconds(10));
+        {
+            y -= speed;
+            this_thread::sleep_for(chrono::milliseconds(5));
         }
         else if ((wasd && IsKeyDown(KEY_S)) || (!wasd && IsKeyDown(KEY_K)))
         {
-            y = y + speed;
-            this_thread::sleep_for(chrono::milliseconds(10));
+            y += speed;
+            this_thread::sleep_for(chrono::milliseconds(5));
         }
 
         if (y <= 0)
@@ -103,19 +108,20 @@ public:
             y = GetScreenHeight() - heigth;
         }
     }
+
+    void Reset()
+    {
+        // reseting positions
+        y = (GetScreenHeight() / 2) - heigth / 2;
+
+        // reseting speeed
+        speed = initial_speed;
+    }
 };
 
 Paddle player1;
 Ball ball;
 Paddle player2;
-
-// void update_ball()
-// {
-//     while (WindowShouldClose() == false)
-//     {
-//         ball.Update();
-//     }
-// }
 
 void update_p1()
 {
@@ -142,65 +148,156 @@ int main()
     ball.radius = 21;
     ball.x = SCREEN_W / 2;
     ball.y = SCREEN_H / 2;
-    ball.speed_x = 7;
-    ball.speed_y = 7;
+    ball.initial_speed = 7;
+    ball.speed_x = ball.initial_speed;
+    ball.speed_y = ball.initial_speed;
 
     player1.wasd = true;
     player1.width = 25;
     player1.heigth = 120;
     player1.x = 10;
     player1.y = SCREEN_H / 2 - player1.heigth / 2;
-    player1.speed = 6;
+    player1.initial_speed = 10;
+    player1.speed = player1.initial_speed;
 
     player2.width = 25;
     player2.heigth = 120;
     player2.x = SCREEN_W - player2.width - 10;
     player2.y = SCREEN_H / 2 - player1.heigth / 2;
-    player2.speed = 6;
+    player2.initial_speed = 10;
+    player2.speed = player2.initial_speed;
 
     cout << "Starting the game" << endl;
 
+    float acceleration = 0.05;
+
     InitWindow(SCREEN_W, SCREEN_H, "Pong Game!");
+    int frameCounter = 0;
+
     SetTargetFPS(60);
 
-    // int semaphore = 0;
-
-    // thread thBall(update_ball);
     thread thP1(update_p1);
     thread thP2(update_p2);
 
+    chrono::time_point<std::chrono::system_clock> start, current;
+
     while (WindowShouldClose() == false)
     {
+        switch (currentScreen)
+        {
+        case LOGO:
+        {
+            frameCounter++; // Count frames
+
+            // Wait for 2 seconds (120 frames) before jumping to TITLE screen
+            if (frameCounter > 120)
+            {
+                currentScreen = TITLE;
+            }
+        }
+        break;
+        case TITLE:
+        {
+            // Press enter to change to GAMEPLAY screen
+            if (IsKeyPressed(KEY_ENTER))
+            {
+                currentScreen = GAMEPLAY;
+                start = chrono::system_clock::now();
+            }
+        }
+        break;
+        case GAMEPLAY:
+        {
+            // Checking for collisions
+            if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player1.x, player1.y, player1.width, player1.heigth}))
+            {
+                ball.speed_x *= -(1 + acceleration);
+                player1.speed *= (1 + acceleration);
+            }
+
+            if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player2.x, player2.y, player2.width, player2.heigth}))
+            {
+                ball.speed_x *= -(1 + acceleration);
+                player2.speed *= (1 + acceleration);
+            }
+
+            // Press enter to change to ENDING screen
+            if (IsKeyPressed(KEY_ENTER))
+            {
+                currentScreen = ENDING;
+            }
+        }
+        break;
+        case ENDING:
+        {
+            // reseting
+            ball.Reset();
+            player1.Reset();
+            player2.Reset();
+
+            // Press enter to return to TITLE screen
+            if (IsKeyPressed(KEY_ENTER))
+            {
+                currentScreen = TITLE;
+            }
+        }
+        break;
+        default:
+            break;
+        }
+
         BeginDrawing();
-
-        ball.Update();
-        // player1.Update();
-        // player2.Update();
-
-        // Checking for collisions
-        if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player1.x, player1.y, player1.width, player1.heigth}))
-        {
-            ball.speed_x *= -1;
-        }
-
-        if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player2.x, player2.y, player2.width, player2.heigth}))
-        {
-            ball.speed_x *= -1;
-        }
-
-        // Drawing
-        ball.Draw();
-        player1.Draw();
-        player2.Draw();
-        DrawText(TextFormat("%f", GetFrameTime()),SCREEN_W/2,SCREEN_H/2,50,WHITE);
         ClearBackground(BLACK);
-        DrawLine(SCREEN_W / 2, 0, SCREEN_W / 2, SCREEN_H, WHITE);
-        EndDrawing();
 
-        // sem_signal(&semaphore);
+        switch (currentScreen)
+        {
+        case LOGO:
+        {
+            // TODO: Draw LOGO screen here!
+            DrawText("LOGO SCREEN", 20, 20, 40, LIGHTGRAY);
+            DrawText("WAIT for 2 SECONDS...", 290, 220, 20, GRAY);
+        }
+        break;
+        case TITLE:
+        {
+            // TODO: Draw TITLE screen here!
+            DrawRectangle(0, 0, SCREEN_W, SCREEN_H, GREEN);
+            DrawText("TITLE SCREEN", 20, 20, 40, DARKGREEN);
+            DrawText("PRESS ENTER or TAP to JUMP to GAMEPLAY SCREEN", 120, 220, 20, DARKGREEN);
+        }
+        break;
+        case GAMEPLAY:
+        {
+            ball.Update();
+
+            // Drawing
+            ball.Draw();
+            player1.Draw();
+            player2.Draw();
+
+            DrawLine(SCREEN_W / 2, 60, SCREEN_W / 2, SCREEN_H, WHITE);
+
+            current = chrono::system_clock::now();
+            score = current - start;
+            DrawText(TextFormat("%.1f", score.count()), (SCREEN_W / 2) - 50, 0, 50, WHITE);
+        }
+        break;
+        case ENDING:
+        {
+            // TODO: Draw ENDING screen here!
+            DrawRectangle(0, 0, SCREEN_W, SCREEN_H, BLUE);
+            DrawText("ENDING SCREEN", 20, 20, 40, DARKBLUE);
+            DrawText("PRESS ENTER or TAP to RETURN to TITLE SCREEN", 120, 220, 20, DARKBLUE);
+            DrawText(TextFormat("SCORE: %.1f", score.count()), (SCREEN_W / 2) - 50, 0, 50, WHITE);
+        }
+        break;
+        default:
+            break;
+        }
+
+        EndDrawing();
     }
 
-    // thBall.join();
     thP1.join();
     thP2.join();
 
